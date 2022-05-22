15f963150d9fc4c0ea8276d4278ecf825927b666
shihuai <tanshihuai@gmail.com>
2022-05-22 21:21:08 +0800

    bt resursion practice

--- dsa basics/binaryTree.py
+++ dsa basics/binaryTree.py
@@ -5,22 +5,18 @@ class TreeNode:
         self.right = None
 
 
-newBT = TreeNode(5)
-newNode3 = TreeNode(3)
-newNode6 = TreeNode(6)
+newBT = TreeNode(1)
 newNode2 = TreeNode(2)
+newNode5 = TreeNode(5)
+newNode3 = TreeNode(3)
 newNode4 = TreeNode(4)
-newNode8 = TreeNode(8)
-newNode1 = TreeNode(1)
-newNode7 = TreeNode(7)
+newNode6 = TreeNode(6)
 
-newBT.left = newNode3
-newBT.right = newNode6
-newNode3.left = newNode2
-newNode3.right = newNode4
-newNode2.left = newNode1
-newNode6.right = newNode8
-newNode8.left = newNode7
+newBT.left = newNode2
+newBT.right = newNode5
+newNode2.left = newNode3
+newNode2.right = newNode4
+newNode5.right = newNode6
 
 
 class queue():
@@ -79,7 +75,66 @@ def levelOrderTraversal(root):
             q.enqueue(node.right)
 
 
+def levelOrder(root):
+    ans = []
+    levels = [root]
+    while root and levels:
+        level_values = []
+        next_level = []
+        for i in levels:
+            level_values.append(i.data)
+            if i.left:
+                next_level.append(i.left)
+            if i.right:
+                next_level.append(i.right)
+        ans.extend([level_values])
+        levels = next_level
+    return ans
 
 
+prev = None
+
+def flatten_tree(root):
+    global prev
+
+    if root == None:
+        return
+    else:
+        flatten_tree(root.right)
+        flatten_tree(root.left)
+        root.left = None
+        root.right = prev
+        prev = root
 
 
+def reverseTree(root):
+    if root == None:
+        return
+    else:
+        reverseTree(root.left)
+        reverseTree(root.right)
+        root.left, root.right = root.right, root.left
+        return root
+
+
+def maxNum(root):
+    if root == None:
+        return -999
+    else:
+        max_left = maxNum(root.left)
+        max_right = maxNum(root.right)
+        return max(root.data, max_left, max_right)
+
+
+def maxDepth(root):
+    if root == None:
+        return 0
+    else:
+        left_depth = maxDepth(root.left) + 1
+        right_depth = maxDepth(root.right) + 1
+        return max(left_depth, right_depth)
+
+
+print(levelOrder(newBT))
+reverseTree(newBT)
+print(maxDepth(newBT))
--- dsa practice/swapNodesInPairs.py
+++ dsa practice/swapNodesInPairs.py
@@ -25,9 +25,9 @@ class Solution(object):
         else:
             second = head.next
             third = head.next.next
-            head.val, second.val = second.val, head.val
-            second.next = self.swapPairs(third)
-            return head
+            second.next = head
+            head.next = self.swapPairs(third)
+            return second
 
 
 def traverse(head):
@@ -36,5 +36,34 @@ def traverse(head):
         head = head.next
 
 
-s = Solution()
-traverse(s.swapPairs(headNode))
+
+
+def mergeSort(arr):
+
+    def merge(arr1, arr2):
+        answer = []
+        i = j = 0
+        while i != len(arr1) and j != len(arr2):
+            if arr1[i] > arr1[j]:
+                answer.append(arr1[j])
+                j += 1
+            else:
+                answer.append(arr2[i])
+                i += 1
+        answer.extend(arr1[i:])
+        answer.extend(arr1[j:])
+        return answer
+
+    if len(arr) == 1:
+        return arr
+    else:
+        mid = len(arr)//2
+        l = mergeSort(arr[:mid])
+        r = mergeSort(arr[mid:])
+        return merge(l, r)
+
+input = [1,5,7,2,1,6,3,1,7,115,62,54]
+
+print(mergeSort(input))
+
+
