836fd0bdc54000f42365c74bfe4797abf5d32a1f
shihuai <tanshihuai@gmail.com>
2023-01-18 00:55:57 +0800

    2sum

---
+++ neetcode/1) Arrays & Hashing/TwoSum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict1 = {}
+        for i in range(len(nums)):
+            toFind = target - nums[i]
+            if dict1.get(toFind) is not None:
+                return [i, dict1[toFind]]
+            else:
+                dict1[nums[i]] = i
+
+
+
+
+
+
+nums = [3,3]
+target = 6
+
+s = Solution()
+print(s.twoSum(nums, target))
